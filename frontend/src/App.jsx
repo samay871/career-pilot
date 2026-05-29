531d531dc3df055028610ae905d63233a9d32b68
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2026-05-29 12:53:04 +0530

    feat(portfolio): Add complete Candy Pop portfolio template #1930

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,6 +28,7 @@ import TextToResume from './pages/TextToResume';
 import About from './components/portfolio/templates/Tech_Startup/About';
 
 
+
 import JobTracker from './pages/JobTracker';
 import { Community, NotFound } from './pages';
 import InterviewPrep from './pages/InterviewPrep';
